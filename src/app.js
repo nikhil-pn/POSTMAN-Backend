15be65f206e77fe5bd98525dad77c5a1646b2dca
nikhilpn725 <nikhilpn725@gmail.com>
2023-01-26 19:29:00 +0530

    routers complete

--- src/app.js
+++ src/app.js
@@ -3,10 +3,17 @@ import cors from "cors";
 import morgan from "morgan";
 
 const app = express();
+
+import { errorHandler, unknowEndpoint } from "./middlewares";
+import requestRouter from "./controllers/request";
+
 //middlewares
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/request", requestRouter);
 
+app.use(unknowEndpoint);
+app.use(errorHandler);
 export default app;
--- src/controllers/request.js
+++ src/controllers/request.js
@@ -42,4 +42,106 @@ requestRouter.get(
   }
 );
 
+requestRouter.get("/:id", async (req, res, next) => {
+  try {
+    const requestId = req.params.id;
+    let foundRequest = await Request.findByPk(requestId);
+
+    if (!foundRequest) {
+      return res.status(404).json({ error: "Not found" });
+    }
+    const savedHeaders = await Header.findAll({
+      where: { requestId: requestId },
+    });
+    //toJSON() is a SQL func
+    foundRequest = foundRequest.toJSON();
+    foundRequest.headers = savedHeaders.toJSON();
+    return res.status(200).json(foundRequest);
+  } catch (error) {
+    return res.status(500).json({ error: error });
+  }
+});
+
+requestRouter.post(
+  "/:id",
+  tokenExtractor,
+  tokenValidator,
+  async (req, res, next) => {
+    try {
+      const decodedToken = validateToken(req, res, next);
+      const user = User.findByPk(decodedToken.id);
+
+      if (!user) {
+        return res.status(404).json({ error: "INvalid user" });
+      }
+      const requestId = req.params.id;
+
+      if (requestId === "0") {
+        const newRequest = await Request.create({
+          url: "",
+          type: "",
+          body: "",
+          UserId: decodedToken.id,
+        });
+        const savedReq = await newRequest.save();
+        return res.status(201).json(savedReq);
+      }
+      let foundReq = await Request.findByPk(requestId);
+
+      if (!foundReq) {
+        return res.status(500).json({ err: err });
+      }
+      //setting default value of body if req are provided
+      let { url = "", type = "", body = "", headers = "" } = req.body;
+
+      foundReq.url = url;
+      foundReq.type = type;
+      foundReq.body = body;
+
+      let savedReq = await foundReq.save();
+
+      for (let header of headers) {
+        await Header.update(
+          {
+            key: header.key || "",
+            value: header.value || "",
+            checked: header.checked || false,
+          },
+          {
+            where: {
+              id: header.id,
+            },
+          }
+        );
+      }
+      savedReq = savedReq.toJSON();
+      let savedHeaders = await Header.findAll({
+        where: {
+          requestId: savedReq.id,
+        },
+      });
+      savedHeaders.headers = savedHeaders;
+
+      return res.status(200).json(savedReq);
+    } catch (error) {
+      return res.status(500).json({ error: error });
+    }
+  }
+);
+
+requestRouter.delete(
+  "/:id/header/:headerId",
+  tokenExtractor,
+  tokenValidator,
+  async (req, res, next) => {
+    const decodedToken = validateToken(req, res, next);
+    const user = User.findByPk(decodedToken.id);
+
+    const headerId = req.params.headerId;
+    const savedHeaders = await Header.findByPk(headerId);
+    await savedHeaders.destroy();
+    return res.status(204).end();
+  }
+);
+
 export default requestRouter;
